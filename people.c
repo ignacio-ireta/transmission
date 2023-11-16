acdf50517897d6ed2622f148dbc9fdf231c48bf2
Ignacio Ireta <ignacio.ireta@outlook.com>
2023-11-16 09:45:39 -0600

    Modified slightly the fix_boundries function to no avail

--- people.c
+++ people.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "people.h"
 
-People new_People(double age, int gender, int status, double time_recovery, double p_x, double p_y, double r_x, double r_y){
+People new_People(double age, int gender, int status, double time_recovery, double p_x, double p_y, double r_x, double r_y) {
   People people;
   people.age = age;
   people.gender = gender;
@@ -13,10 +13,8 @@ People new_People(double age, int gender, int status, double time_recovery, doub
   people.velocity[1] = r_y;
   people.updated = false;
   return people;
-
 }
 
-void print_People(People people){
-  printf("%f\t%i\t%i\t%f\t[%f,%f]\t[%f,%f]\n",people.age, people.gender, people.status,people.time_recovery, people.position[0],people.position[1],people.velocity[0],people.velocity[1]);
-
+void print_People(People people) {
+  printf("%f\t%i\t%i\t%f\t[%f,%f]\t[%f,%f]\n", people.age, people.gender, people.status, people.time_recovery, people.position[0], people.position[1], people.velocity[0], people.velocity[1]);
 }
--- population.c
+++ population.c
@@ -4,35 +4,33 @@
 #include "population.h"
 #include "people.h"
 
-boolean check(Population p, double x, double y, double radii){
+boolean check(Population p, double x, double y, double radii) {
   double distance;
   int i;
-  for(i=0; i < p.iterator;i++){
-      distance = sqrt(pow(p.people[i].position[0]-x,2) + pow(p.people[i].position[1]-y,2));
-      if (distance <= radii){
-	return false;
-      }
+  for (i = 0; i < p.iterator; i++) {
+    distance = sqrt(pow(p.people[i].position[0] - x, 2) + pow(p.people[i].position[1] - y, 2));
+    if (distance <= radii) {
+      return false;
+    }
   }
-  return true;  
+  return true;
 }
 
-Population new_Population(char name[], int size){
+Population new_Population(char name[], int size) {
   Population p;
-  sprintf(p.name,"%s",name);
-  p.people = malloc(size*sizeof(People));
+  sprintf(p.name, "%s", name);
+  p.people = malloc(size * sizeof(People));
   p.size = size;
   p.iterator = 0;
   return p;
 }
 
-
-int add_people_to_population(Population *p, People a){
-  if (p->iterator < p->size){
+int add_people_to_population(Population *p, People a) {
+  if (p->iterator < p->size) {
     p->people[p->iterator] = a;
     p->iterator++;
     return 1;
-  }else{
+  } else {
     return 0;
   }
 }
-
